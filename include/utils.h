f662027b8c4cb777789681ad44ae1e9ef11f2728
athaydaooo <athaydaooo@gmail.com>
2024-12-03 19:36:02 -0300

    fix: string implementation

--- include/utils.h
+++ include/utils.h
@@ -4,7 +4,12 @@
 #include <gmp.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 void print_variable(const char *var_name, mpz_t value, bool extra_newline);
+
+void string_to_mpz(mpz_t result, const char *str);
+
+void mpz_to_string(char *str, mpz_t value);
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -1,4 +1,5 @@
 #include "utils.h"
+#include <stdlib.h>
 
 void print_variable(const char *var_name, mpz_t value, bool extra_newline) {
     printf("%s = ", var_name);
@@ -12,4 +13,25 @@ void print_variable(const char *var_name, mpz_t value, bool extra_newline) {
         printf("\n");
     }
     
+}
+
+void string_to_mpz(mpz_t result, const char *str) {
+    size_t len = strlen(str);
+    mpz_set_ui(result, 0);  // Inicializa result como 0
+
+    // Converter cada caractere da string para um número e concatenar
+    for (size_t i = 0; i < len; i++) {
+        mpz_mul_ui(result, result, 256);  // Multiplica por 256 (base para os caracteres ASCII)
+        mpz_add_ui(result, result, (unsigned char)str[i]);  // Adiciona o valor do caractere
+    }
+}
+
+void mpz_to_string(char *str, mpz_t value) {
+    size_t length = mpz_sizeinbase(value, 256) + 1;
+    char *buffer = (char *)malloc(length);
+
+    mpz_export(buffer, &length, 1, 1, 0, 0, value);
+    strncpy(str, buffer, length);
+
+    free(buffer);
 }
